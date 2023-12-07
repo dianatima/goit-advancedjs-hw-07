f88918d3be79c28b716df7680b6aab74f9754bce
Yulia <nayanzina@gmail.com>
2023-12-08 00:48:34 +0200

    hw-07

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random()   
+    }
+
+    getSignature(): number {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key){
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    public door: boolean = false;
+    public tenants: Person[] = [];
+    constructor(public key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person)
+        }
+    }
+
+   public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+    
+    public openDoor(key: Key): void {
+        if (key.getSignature === this.key.getSignature) {
+            this.door = true;
+        }
+    }
+  
+}
+    
 const key = new Key();
 
 const house = new MyHouse(key);
